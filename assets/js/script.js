f51f10ae1823675faae73768730871b9d8c1096c
Matheus Bezerra <mabezerra2@gmail.com>
2022-11-12 14:47:35 -0400

    adicionando eslint na aplicação

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-"use strict";
-const MAGICNUMBER = 10;
-const clockDigital = document.querySelector('.digital');
-const secondElement = document.querySelector('.p_s');
-const minuteElement = document.querySelector('.p_m');
-const hoursElement = document.querySelector('.p_h');
+'use strict'
+const MAGICNUMBER = 10
+const clockDigital = document.querySelector('.digital')
+const secondElement = document.querySelector('.p_s')
+const minuteElement = document.querySelector('.p_m')
+const hoursElement = document.querySelector('.p_h')
 const updateClock = () => {
-    const now = new Date();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const second = now.getSeconds();
-    clockDigital.innerHTML = `${formatedTime(hour)}:${formatedTime(minute)}:${formatedTime(second)}`;
-};
-const formatedTime = (time) => time < MAGICNUMBER ? `0${time}` : time;
-setInterval(updateClock, 1000);
+  const now = new Date()
+  const hour = now.getHours()
+  const minute = now.getMinutes()
+  const second = now.getSeconds()
+  clockDigital.innerHTML = `${formatedTime(hour)}:${formatedTime(minute)}:${formatedTime(second)}`
+}
+const formatedTime = (time) => time < MAGICNUMBER ? `0${time}` : time
+setInterval(updateClock, 1000)
